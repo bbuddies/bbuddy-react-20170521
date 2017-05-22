824b97a91cd71f0dab1c32dbecd2948e43ac089e
LittleLin <walle.kobayashi@gmail.com>
2017-05-22 11:23:29 +0800

    Add updating logic & unit test for addBudgetPagePresenter

--- test/presenters/addBudgetPagePresenterTest.js
+++ test/presenters/addBudgetPagePresenterTest.js
@@ -1,10 +1,10 @@
 import {AddBudgetPagePresenter} from '../../app/presenters/addBudgetPagePresenter'
 
 describe('AddBudgetPagePresenter', () => {
-  context('Save budget', () => {
+  context('Add budget', () => {
     let addBudgetSpy, goBackSpy
     beforeEach(() => {
-      let props = {addBudget: () => {}, goBack: () => {}}
+      let props = {addBudget: () => {}, updateBudget: () => {}, goBack: () => {}, loadBudgets: () => {}}
       addBudgetSpy = sinon.stub(props, 'addBudget').yields()
       goBackSpy = sinon.spy(props, 'goBack')
       let presenter = new AddBudgetPagePresenter(props)
@@ -17,4 +17,38 @@ describe('AddBudgetPagePresenter', () => {
       goBackSpy.should.be.called
     })
   })
+  
+  context('Update budget', () => {
+    it('save by action', () => {
+      // arrange
+      let props = {addBudget: () => {}, updateBudget: () => {}, goBack: () => {}, loadBudgets: () => {}}
+      let addBudgetSpy = sinon.stub(props, 'addBudget').yields()
+      let loadBudgets = sinon.spy(props, 'loadBudgets')
+      let goBackSpy = sinon.spy(props, 'goBack')
+      
+      // act
+      let presenter = new AddBudgetPagePresenter(props)
+      presenter.save({month: '2017-05', amount: 300})
+
+      // assert
+      loadBudgets.should.be.called
+      addBudgetSpy.should.be.calledWith({month: '2017-05', amount: 300}, sinon.match.any)
+    })
+
+    it('invoke update budget', () => {
+      // arrange
+      let props = {addBudget: () => {}, updateBudget: () => {}, goBack: () => {}, loadBudgets: () => {}}
+      let updateBudget = sinon.stub(props, 'updateBudget').yields();
+      let loadBudgets = sinon.stub(props, 'loadBudgets');
+      
+      loadBudgets.returns({1: {month: '2017-05', amount: 1000}})
+      
+      // act
+      let presenter = new AddBudgetPagePresenter(props)
+      presenter.save({month: '2017-05', amount: 300})
+
+      // assert
+      updateBudget.should.be.called
+    })
+  })  
 })
